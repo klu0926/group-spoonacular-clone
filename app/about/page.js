93584387cf2f2031411384d6bc2f932a743252fd
CL <leungc22@gmail.com>
2025-07-30 19:40:24 -0400

    cards for aboutpage added

--- app/about/page.js
+++ app/about/page.js
@@ -12,12 +12,33 @@ export default function AboutPage() {
 				<header className="text-center mb-8">
 					<h1 className="text-4xl font-bold text-gray-800 mb-2">The Team</h1>
 				</header>
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+				<div className="flex flex-wrap justify-center gap-8">
+					<BioCard
+						name={"Darryl Lecraw"}
+						image={"https://avatars.githubusercontent.com/u/6304758"}
+						bio={"Bio for Darryl"}
+						link={"https://github.com/BossClaw"} />
 					<BioCard
 						name={"Clarance Leung"}
 						image={"https://avatars.githubusercontent.com/u/194448761"}
-						bio={"Bio for Clarance"}
-						link={"https://github.com/kn-mn"}
+						bio={"Bio for CL"}
+						link={"https://github.com/kn-mn"} />
+					<BioCard
+						name={"Kuo Yu Lu"}
+						image={"https://avatars.githubusercontent.com/u/106499794"}
+						bio={`Former cinematic wizard turned code-slinging web dev. Built over 100 epic battle cutscenes. now battles bugs in JavaScript. Part-time SQL whisperer, full-time front-end sorcerer. Can debug faster than you can say “unexpected token.” Studying law, psychology, and car resale all at once because… why not? Dreams in Tailwind CSS and occasionally in APA 7 format.`}
+						link={"https://github.com/klu0926"} />
+					<BioCard
+						name={"Noah Park"}
+						image={"#"}
+						bio={"Bio for Noah"}
+						link={"https://github.com/noahp211"}
+					/>
+					<BioCard
+						name={"Jasmine Sanders"}
+						image={"https://avatars.githubusercontent.com/u/172424810"}
+						bio={"Bio for Jasmine"}
+						link={"https://github.com/Jaysandja3y"}
 					/>
 				</div>
 			</div>
--- app/components/BioCards.js
+++ app/components/BioCards.js
@@ -1,26 +1,32 @@
 "use client"
 
+import { FaGithub } from "react-icons/fa";
 import { useState } from "react";
 
 export default function BioCard({ name, image, bio, link }) {
 
     const [flipped, setFlipped] = useState(false);
+    const CARD_BASE = "absolute inset-0 rounded-xl bg-white shadow-lg p-4 flex flex-col items-center justify-center [backface-visibility:hidden]";
+
 
     return (
         <div className="cursor-pointer group [perspective:1000px] w-64 h-80" onClick={() => setFlipped(prev => !prev)}>
             <div className={`relative w-full h-full duration-700 [transform-style:preserve-3d] ${flipped ? "[transform:rotateY(180deg)]" : ""}`}>
                 {/* Front */}
-                <div className="absolute inset-0 [backface-visibility:hidden] flex flex-col items-center shadow-lg">
-                    <img src={image} alt={name} className="" />
-                    <h2 className="text-gray-800">{name}</h2>
+                <div className={`${CARD_BASE} justify-center`}>
+                    <img src={image || null } alt={`${name} Avatar`} className="h-24 w-24 rounded-full object-cover" />
+                    <h2 className="text-gray-800 mt-4 text-xl font-semibold">{name}</h2>
                 </div>
                 {/* Back */}
-                <div className='absolute inset-0 [transform:rotateY(180deg)] [backface-visibility:hidden] flex flex-col items-center shadow-lg'>
-                    <h2 className="text-gray-800">{name}</h2>
-                    <p className="text-gray-800">{bio}</p>
-                    <a href={link} className="block w-6 h-6"><img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" alt="GitHub Link" className="w-full h-full"/></a>
+                <div className={`${CARD_BASE} [transform:rotateY(180deg)] justify-between`}>
+                    <h2 className="text-gray-800 mt-4 text-xl font-semibold">{name}</h2>
+                    <p className="text-gray-800 mt-2 text-xs text-center line-clamp-15">{bio}</p>
+                    {/* <a href={link} className="block w-6 h-6"><img fill={true} src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" alt="GitHub Link" className="w-full h-full"/></a> */}
+                    <a href={link} target="_blank" rel="noopener noreferrer" aria-label={`${name} GitHub`} className="text-black mt-4 w-6 h-6 hover:scale-110 transition-transform" onClick={e => { e.stopPropagation(); }}>
+                        <FaGithub className="w-full h-full" />
+                    </a>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
