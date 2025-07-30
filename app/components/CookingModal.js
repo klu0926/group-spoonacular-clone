373ff9ea68328b0f486f982db92f3e3e5fdbd48f
Darryl <darryl@racecar.com>
2025-07-29 20:09:24 -0400

    stubs: Modal, RecipeView, RecipeList

---
+++ app/components/CookingModal.js
@@ -0,0 +1,17 @@
+// AUTHOR: Darryl, Jasmine
+
+export default function CookingModal({ recipe, isOpen, onClose }) {
+	// TODO - RECIPE DTA
+
+	// TODO - WHETHER ISOPEN
+
+	// TODO - CLOSE HANDLER
+
+	return (
+		<div>
+			{/* TODO - MODAL FOR RECIPE DETAILS VIEW */}
+			<span>MODAL!!</span>
+			<button>Close</button>
+		</div>
+	);
+}
