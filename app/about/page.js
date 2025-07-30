963826248927115ff131517576a8d17a6906bc01
CL <leungc22@gmail.com>
2025-07-30 18:42:56 -0400

    flip card function added

--- app/about/page.js
+++ app/about/page.js
@@ -7,14 +7,20 @@ import BioCard from "../components/BioCards";
 export default function AboutPage() {
 	// TODO - NOTHING DYNAMIC ON ABOUT PAGE...
 	return (
-		<div className="">
-			<div>About</div>
-			<BioCard
-				name=""
-				image=""
-				bio=""
-				link=""
-			/>
+		<div className="min-h-screen bg-blue-800 py-8">
+			<div className="container mx-auto my-8 px-4 py-8 bg-white">
+				<header className="text-center mb-8">
+					<h1 className="text-4xl font-bold text-gray-800 mb-2">The Team</h1>
+				</header>
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+					<BioCard
+						name={"Clarance Leung"}
+						image={"https://avatars.githubusercontent.com/u/194448761"}
+						bio={"Bio for Clarance"}
+						link={"https://github.com/kn-mn"}
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
--- app/components/BioCards.js
+++ app/components/BioCards.js
@@ -1,17 +1,26 @@
 "use client"
 
-return (
-    <div className="" onClick={handleFlip}>
-        {/* Front */}
-        <div className="">
-            <img src={image} alt="placeholder" className="" />
-            <h2 className="">{name}</h2>
+import { useState } from "react";
+
+export default function BioCard({ name, image, bio, link }) {
+
+    const [flipped, setFlipped] = useState(false);
+
+    return (
+        <div className="cursor-pointer group [perspective:1000px] w-64 h-80" onClick={() => setFlipped(prev => !prev)}>
+            <div className={`relative w-full h-full duration-700 [transform-style:preserve-3d] ${flipped ? "[transform:rotateY(180deg)]" : ""}`}>
+                {/* Front */}
+                <div className="absolute inset-0 [backface-visibility:hidden] flex flex-col items-center shadow-lg">
+                    <img src={image} alt={name} className="" />
+                    <h2 className="text-gray-800">{name}</h2>
+                </div>
+                {/* Back */}
+                <div className='absolute inset-0 [transform:rotateY(180deg)] [backface-visibility:hidden] flex flex-col items-center shadow-lg'>
+                    <h2 className="text-gray-800">{name}</h2>
+                    <p className="text-gray-800">{bio}</p>
+                    <a href={link} className="block w-6 h-6"><img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" alt="GitHub Link" className="w-full h-full"/></a>
+                </div>
+            </div>
         </div>
-        {/* Back */}
-        <div className="">
-            <h2 className="">{name}</h2>
-            <p className="">{bio}</p>
-            <button className="">{link}</button>
-        </div>
-    </div >
-)
+    );
+}
